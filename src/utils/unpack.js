de2214cdf45bc6c6ce0416ca148f75a44dda7b4d
Robb Traister <robb.traister@sciencelogic.com>
2020-01-07 11:20:02 -0500

    be clever with unpack

--- src/utils/unpack.js
+++ src/utils/unpack.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function unpack(mod) {
-  return mod && mod.__esModule && mod.default ? mod.default : mod
+  return mod && mod.__esModule ? Object.assign(mod.default || {}, mod) : mod
 }
 
 module.exports = unpack
