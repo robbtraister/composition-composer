36fef974268e1c167d3221c38edd6c635cdb0d2b
Robb Traister <robb.traister@sciencelogic.com>
2020-01-10 21:05:55 -0500

    add useResource to component exports

--- src/components/resource/client.ts
+++ src/components/resource/client.ts
@@ -1,5 +1,9 @@
 'use strict'
 
+export const useResource = () => {
+  throw new Error('`useResource` hook is unavailable in the browser')
+}
+
 export const Resource = () => {
   throw new Error('`Resource` component is unavailable in the browser')
 }
--- src/components/resource/index.ts
+++ src/components/resource/index.ts
@@ -1,10 +1,17 @@
 'use strict'
 
-import { Resource as ClientResource } from './client'
-import { Resource as ServerResource } from './server'
+import {
+  Resource as ClientResource,
+  useResource as useClientResource
+} from './client'
+import {
+  Resource as ServerResource,
+  useResource as useServerResource
+} from './server'
 
 import { isClient } from '../utils'
 
 export const Resource = isClient ? ClientResource : ServerResource
+export const useResource = isClient ? useClientResource : useServerResource
 
 export default Resource
