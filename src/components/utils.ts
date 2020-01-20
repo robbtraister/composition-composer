5beb59479dcaf4dd80a70856f22b579edbd06eea
Robb Traister <robb.traister@gmail.com>
2020-01-20 10:03:17 -0500

    update isClient logic; cleanup ts overrides

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,6 +1,10 @@
 'use strict'
 
-export const isClient = typeof process === 'undefined'
+export const isClient =
+  typeof process === 'undefined' ||
+  !!process.browser ||
+  !process.version ||
+  process.argv.length === 0
 
 export function getDescendants(node: { children?: React.ReactNode } = {}) {
   const children = [].concat(node.children || [])
--- src/definitions/overrides.d.ts
+++ src/definitions/overrides.d.ts
@@ -1,18 +1,11 @@
-declare module 'cluster' {
-  interface ClusterSettings {
-    workerCount?: number
-  }
-}
-
-declare namespace Express {
-  interface Request {
-    csrfToken?: Function
-    user?: object
-  }
-}
-
 declare namespace JSX {
   interface IntrinsicElements {
     'composition:styled-components': any
+  }
+}
+
+declare namespace NodeJS {
+  interface Process {
+    browser?: boolean
   }
 }
